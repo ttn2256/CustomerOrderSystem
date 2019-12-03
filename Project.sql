c609179194081df8c5f0eac48379fd16a7fc13d0
Tuan Nguyen <ttn2256@utexas.edu>
2019-12-03 10:34:23 -0800

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
 --
--- Host: localhost    Database: CS5200
+-- Host: localhost    Database: TuanProject
 -- ------------------------------------------------------
 -- Server version	8.0.17
 
